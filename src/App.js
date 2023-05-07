b0786b874e9f6f682bee1b4cbb724c3647f6c7dd
Boban Milosevik <bobavtokom888@gmail.com>
2023-05-07 21:55:36 +0200

    changed almost everything

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
-import TodoList from './TodoList';
 
 function App() {
+  const[newTodo, setNewTodo] = useState("");
+  const[todos,setTodos] = useState([]);
+  const [completed, setCompleted] = useState(false);
+
+  function addTodo() {
+    if (!newTodo){
+     alert("Please enter todo item!");
+    }
+   const todo = {
+    id: Math.floor(Math.random() * 1000),
+    value: newTodo,
+   }
+   setTodos(oldList => [...oldList, todo]);
+   setNewTodo("");
+  }
+  function deleteTodo(id) {
+     const deletedTodo = todos.filter(todo => todo.id !== id);
+     setTodos(deletedTodo);
+  }
+  const completeTodo = () => {
+    setCompleted(!completed);
+  };
   return (
     <div className="App">
-     <TodoList />
+     <h1>Todo list</h1>
+     <input type="text"
+     placeholder='Add todo'
+     value={newTodo}
+     onChange={e => setNewTodo(e.target.value)}
+     />
+     <button onClick={() => addTodo()}>Add</button>
+     <ul>
+     {todos.map(todo => {
+      return (
+        <li key={todo.id}><span style={{ textDecoration: completed ? 'line-through' : 'none' }}>{todo.value} </span>
+        <button className="button-delete" onClick={() => deleteTodo(todo.id)}>Delete</button>
+        <button className="button-complete" onClick={completeTodo}>{completed ? 'Incomplete' : 'Complete'}</button>
+        </li>
+      )
+     })}
+     </ul>
     </div>
   );
 }
